6e6f89062aa72637fc54b44cd01d6e011e13cd8b
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2024-12-17 14:35:57 -0600

    Adding editing capabilities

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override")
 const Campground = require("./models/campground")
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -22,6 +23,7 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -54,6 +56,17 @@ app.get("/campgrounds/:id", async (req, res) =>{
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", {campground})
 
+})
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render("campgrounds/edit", {campground})
+})
+
+app.put("/campgrounds/:id", async(req, res) => {
+   const {id} = req.params;
+   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}) // saving as an object in the EJS file edit.ejs allows us to access the object from the body
+   res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.listen(3000, () => {
