9e79faa6c351bdc70a0e1579d02f12851e5b0051
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-01-20 15:03:30 -0600

    Deleting Reviews

--- app.js
+++ app.js
@@ -115,6 +115,13 @@ app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async (req, res)
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async(req,res) => {
+    const {id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull:{reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
 // A path that doesn't exist will end up here
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page Not Found!!", 404))
