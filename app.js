d04901b51b82c83068b4d3fa28e20a99e859be1e
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-01-28 13:04:05 -0600

    Breaking out reviews routes using Express Router

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require('ejs-mate');
-const {campgroundSchema, reviewSchema} = require("./schemas.js")
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError")
 const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const Review = require("./models/review")
 const { error } = require("console");
 
 const campgrounds = require("./routes/campgrounds")
+const reviews = require("./routes/reviews")
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -34,39 +31,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
 
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(",")
-        throw new ExpressError(msg, 400)
-    } else {
-        next(); 
-    }
-
-}
-
-app.use("/campgrounds", campgrounds)
+app.use("/campgrounds", campgrounds);
+app.use('/campgrounds/:id/reviews', reviews); //if we need access to this is on the router we have to use the mergeParams value in the router 
 
 app.get("/", (req, res) => {
     res.render("home");
 })
 
-
-app.post("/campgrounds/:id/reviews", validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async(req,res) => {
-    const {id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull:{reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 // A path that doesn't exist will end up here
 app.all("*", (req, res, next) => {
