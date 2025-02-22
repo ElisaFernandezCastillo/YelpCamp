002db0d0ac54039c7d1dad99e93f3d25c79e1d36
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-02-22 13:24:38 -0600

    isLoggedIn Middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError")
 const Campground = require("../models/campground");
 const {campgroundSchema} = require("../schemas.js")
+const {isLoggedIn} = require("../middleware")
 
 const validateCampground = (req, res, next) => {
     const {error} = campgroundSchema.validate(req.body);
@@ -28,12 +29,12 @@ router.get("/", catchAsync(async (req, res) => {
 }))
 
 // This is the form to make a new campground, this should come first compared to /campgrounds/:id otherwise we are directed here
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("campgrounds/new")
 })
 
 // we can do a validation of the campground at this level calling the validateCampground middleware
-router.post("/", validateCampground, catchAsync(async (req,res, next) => {
+router.post("/", isLoggedIn, validateCampground, catchAsync(async (req,res, next) => {
     // if(!req.body.campground) throw new ExpressError("Invalid Campground", 400);
     //res.send(req.body); //in order for the body to be parsed and transferred, we need to add a library that does the parsing router.use(express.urlencoded)
     // The validations of the objects we recieve are going to be made using the JOI library
@@ -54,7 +55,7 @@ router.get("/:id", catchAsync(async (req, res) =>{
 
 }))
 
-router.get("/:id/edit", catchAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     if(!campground){
         req.flash('error', "Cannot find that campground!")
@@ -63,14 +64,14 @@ router.get("/:id/edit", catchAsync(async (req, res) => {
     res.render("campgrounds/edit", {campground})
 }))
 
-router.put("/:id", validateCampground, catchAsync(async(req, res) => {
+router.put("/:id", isLoggedIn, validateCampground, catchAsync(async(req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}) // saving as an object in the EJS file edit.ejs allows us to access the object from the body
     req.flash("success", "Successfully updated campground!")
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete("/:id", catchAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, catchAsync(async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash("success", "Successfully deleted campground!")
