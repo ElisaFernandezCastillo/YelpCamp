267c9604ed91aed0bb223ed1ce9e96f9b9b03aa3
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-02-22 14:09:01 -0600

    Adding Logout

--- routes/users.js
+++ routes/users.js
@@ -34,4 +34,15 @@ router.post("/login", passport.authenticate("local", {failureFlash: true, failur
 
 })
 
+router.get("/logout", (req, res, next) => {
+    req.logout(function (err) {
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "Goodbye!!")
+        res.redirect("/campgrounds")
+    });
+    
+})
+
 module.exports = router;
