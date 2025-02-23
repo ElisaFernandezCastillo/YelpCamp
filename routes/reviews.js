34989480fb22f3c45bec74b3e5b2d8ee2fc3ce90
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-02-23 14:26:54 -0600

    Adding authorization middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,20 +3,9 @@ const router = express.Router({mergeParams: true}); //this parameter is needed s
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review")
-const {reviewSchema} = require("../schemas.js")
+const {validateReview} = require("../middleware")
 
 const ExpressError = require("../utils/ExpressError")
-
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(",")
-        throw new ExpressError(msg, 400)
-    } else {
-        next(); 
-    }
-
-}
 
 router.post("/", validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
