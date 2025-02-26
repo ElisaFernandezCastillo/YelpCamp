5c54352b724383fd38fe04e3168a9b8037ad6587
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-02-25 19:34:52 -0600

    Adding reviews authorization

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const {campgroundSchema, reviewSchema} = require("./schemas.js")
 const ExpressError = require("./utils/ExpressError")
 const Campground = require("./models/campground");
+const Review = require("./models/review")
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
@@ -22,6 +23,17 @@ module.exports.isAuthor = async(req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)){
+        req.flash("error", "You don't have permission to do this");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    // The name of the parameters must match what is written in the route
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)){
         req.flash("error", "You don't have permission to do this");
         return res.redirect(`/campgrounds/${id}`);
     }
