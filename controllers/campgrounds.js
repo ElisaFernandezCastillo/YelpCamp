a2221757f83abdbfa4a35bd988ca23c7a99911e6
ElisaFernandezCastillo <elisafernandezcastillo17@gmail.com>
2025-02-26 13:59:44 -0600

    Refactoring Campgrounds Controller

---
+++ controllers/campgrounds.js
@@ -0,0 +1,60 @@
+const Campground = require("../models/campground");
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("campgrounds/new")
+}
+
+module.exports.createCampground = async (req,res, next) => {
+    // if(!req.body.campground) throw new ExpressError("Invalid Campground", 400);
+    //res.send(req.body); //in order for the body to be parsed and transferred, we need to add a library that does the parsing router.use(express.urlencoded)
+    // The validations of the objects we recieve are going to be made using the JOI library
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash("success", "Successfully made a new campground!")
+    res.redirect(`/campgrounds/${campground._id}`)
+    
+}
+
+module.exports.showCampground = async (req, res) =>{
+    const campground = await Campground.findById(req.params.id)
+    .populate({path: "reviews", 
+        populate: {path: "author"}}).
+        populate("author");
+     
+    if(!campground){
+        req.flash('error', "Cannot find that campground!")
+        return res.redirect("/campgrounds")
+    }
+    res.render("campgrounds/show", {campground})
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    if(!campground){
+        req.flash('error', "Cannot find that campground!")
+        return res.redirect("/campgrounds")
+    }
+    res.render("campgrounds/edit", {campground})
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const {id} = req.params;
+     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}) // saving as an object in the EJS file edit.ejs allows us to access the object from the body
+    req.flash("success", "Successfully updated campground!")
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+    
+
+
+module.exports.deleteCampground = async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground!")
+    res.redirect("/campgrounds") 
+}
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const {isLoggedIn, isAuthor, validateCampground} = require("../middleware")
@@ -12,65 +13,20 @@ await camp.save();
 res.send(camp);
 }) */
 
-router.get("/", catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-}))
+router.get("/", catchAsync(campgrounds.index));
 
 // This is the form to make a new campground, this should come first compared to /campgrounds/:id otherwise we are directed here
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("campgrounds/new")
-})
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
 // we can do a validation of the campground at this level calling the validateCampground middleware
-router.post("/", isLoggedIn, validateCampground, catchAsync(async (req,res, next) => {
-    // if(!req.body.campground) throw new ExpressError("Invalid Campground", 400);
-    //res.send(req.body); //in order for the body to be parsed and transferred, we need to add a library that does the parsing router.use(express.urlencoded)
-    // The validations of the objects we recieve are going to be made using the JOI library
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash("success", "Successfully made a new campground!")
-    res.redirect(`/campgrounds/${campground._id}`)
-    
-}))
+router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get("/:id", catchAsync(async (req, res) =>{
-    const campground = await Campground.findById(req.params.id)
-    .populate({path: "reviews", 
-        populate: {path: "author"}}).
-        populate("author");
-     
-    console.log(campground);
-    if(!campground){
-        req.flash('error', "Cannot find that campground!")
-        return res.redirect("/campgrounds")
-    }
-    res.render("campgrounds/show", {campground})
+router.get("/:id", catchAsync(campgrounds.showCampground));
 
-}))
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    if(!campground){
-        req.flash('error', "Cannot find that campground!")
-        return res.redirect("/campgrounds")
-    }
-    res.render("campgrounds/edit", {campground})
-}))
+router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(async(req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}) // saving as an object in the EJS file edit.ejs allows us to access the object from the body
-    req.flash("success", "Successfully updated campground!")
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash("success", "Successfully deleted campground!")
-    res.redirect("/campgrounds") 
-}))
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
